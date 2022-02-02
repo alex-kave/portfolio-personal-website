837f548d080c9c60643b83bac5a36301b87e5955
aledri <a.driessen@agentur-fahrenheit.de>
2022-02-02 17:05:22 +0100

    [Feature]: refactored back top button component

---
+++ src/components/BackTop/BackTop.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+let windowY = 0;
+
+const scrollTop = () =>{
+    window.scrollTo(0,0)
+}
+</script>
+<svelte:window bind:scrollY={windowY} />
+<svelte:options tag="back-top" />
+<div class="backtop" class:backtop--visible={windowY > 600} on:click={scrollTop}>
+    <feather-icon icon="chevron-up" svgwidth="60" svgheight="60"></feather-icon>
+</div>
+<style lang="scss">
+    .backtop{
+        position: fixed;
+        background-color: $secondary;
+        left: 4rem;
+        bottom: 4rem;
+        opacity: 0;
+        &:hover{
+            background-color: darken($secondary, 10%);
+        }
+        &--visible{
+            opacity: 1;
+            transition: opacity 0.2s ease-in;
+            &:hover{
+                cursor: pointer;
+            }
+        }
+    }
+</style>
--- src/components/shared/FeatherIcon.svelte
+++ src/components/shared/FeatherIcon.svelte
@@ -3,14 +3,21 @@
 # Import
 --------------------------------------------------------------*/
 import feather from "feather-icons"
+import { onMount } from "svelte";
 
 /*--------------------------------------------------------------
 # Vars
 --------------------------------------------------------------*/
 export let icon:string;
-export let attrs;
+export let svgwidth:string;
+export let svgheight:string;
+
 let svg;
-$: svg = icon ? feather.icons[icon].toSvg(attrs) : '';
+
+onMount(()=>{
+    svg = icon ? feather.icons[icon].toSvg({"width": svgwidth, "height": svgheight}) : '';
+})
+
 </script>
 <svelte:options tag="feather-icon" />
 <span>{@html svg}</span>
